abee9eae923a5406db9ab4044f56735b8c6e4bdd
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-03 14:26:37 -0700

    1-feat. Initialized Java Project

---
+++ fun-plugin/src/main/java/plugin/first/App.java
@@ -0,0 +1,13 @@
+package plugin.first;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
