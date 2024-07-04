7bf1a2479126420f9e62224638e81962ea483b8d
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-03 22:03:07 -0700

    5-feat. Successfully deployed a plugin

--- src/main/java/plugin/first/App.java
+++ src/main/java/plugin/first/App.java
@@ -2,15 +2,7 @@ package plugin.first;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
-/**
- * Hello world!
- *
- */
 public class App extends JavaPlugin {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
-    }
-
     @Override
     public void onEnable() {
         getLogger().info("Hello World!");
