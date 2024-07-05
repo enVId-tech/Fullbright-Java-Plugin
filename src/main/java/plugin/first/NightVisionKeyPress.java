f6f34f66ca03817adf18a03cc72a26cd573fabe6
Erick Tran <thelittlebotengineer@gmail.com>
2024-07-04 19:03:05 -0700

    10-feat. Added serverside option of night vision toggle

--- src/main/java/plugin/first/NightVisionKeyPress.java
+++ src/main/java/plugin/first/NightVisionKeyPress.java
@@ -8,13 +8,12 @@ import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
 
 public class NightVisionKeyPress implements Listener {
-    // This method will be called when a player presses a key, and will toggle night vision on and off if the key pressed is "V"
     @EventHandler
     public void onPlayerInteract(PlayerInteractEvent event) {
         Player player = event.getPlayer();
-        if (event.getAction().name().contains("RIGHT_CLICK_AIR") && player.getInventory().getItemInMainHand().getType().name().contains("DIAMOND_SWORD")) {
-            System.out.println("Command executed by " + player.getName());
-            System.out.println(player.getName() + " has night vision: " + player.hasPotionEffect(PotionEffectType.NIGHT_VISION));
+        if ((event.getAction().name().contains("RIGHT_CLICK") || event.getAction().name().contains("LEFT_CLICK"))
+                && player.getInventory().getItemInMainHand().getType().name().isBlank()
+                && player.getInventory().getItemInOffHand().getType().name().isBlank()) {
 
             if (player.hasPotionEffect(PotionEffectType.NIGHT_VISION)) {
                 player.removePotionEffect(PotionEffectType.NIGHT_VISION);
